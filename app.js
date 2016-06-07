8b31305b09c83779ef0dded62938f72d5c52fbe7
Ryan <ryebread23@users.noreply.github.com>
2016-06-07 10:30:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 <script>
   $(document).ready(function() {
     $("#target1").css("color", "red");
-    $("#target1").prop("disabled", true);
-
+    $("#target4").html("<em>#target4</em>");
+    
   });
 </script>
 
